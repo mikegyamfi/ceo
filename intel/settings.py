7b5ebb240509299b06361568e62773645b837c56
mikegyamfi <codeage20@gmail.com>
2024-02-02 11:51:32 +0000

    initial commit

--- intel/settings.py
+++ intel/settings.py
@@ -127,7 +127,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'HOST': config("DATABASE_HOST"),
         'PORT': config("DATABASE_PORT"),
-        'NAME': 'ceo-db',
+        'NAME': 'db',
         'USER': config("DATABASE_USERNAME"),
         'PASSWORD': config("DATABASE_PASSWORD"),
         'OPTIONS': {
